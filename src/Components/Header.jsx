f3336388ccb7dd4c9232e156ba4492b1429b4147
Marcel Uchenna <obiefunamarcel@gmail.com>
2022-08-29 17:58:40 -0700

    testing continous intergration with netlify and github

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     return (
         <header className='header'>
             <img className='img' src={img} alt='Profile' />
-            <h1 className='heading-name'> My react profile</h1>
+            <h1 className='heading-name'>Obiefuna Marcel</h1>
             <h3 className='heading-title'>Fullstack developer</h3>
             <button className='link-btn' >uche.com</button>
             <div className='btn-container'>
@@ -16,8 +16,8 @@ export default function Header() {
             </div>
             <h2>About</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, atque. Perspiciatis rem consequuntur ex neque eveniet sit voluptatum doloribus, eius ipsa</p>
-        <h2>Interests</h2>
-       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, natus neque officiis, sapiente provident quod expedita repellat in necessitatibus ipsum accusan</p>
+            <h2>Interests</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, natus neque officiis, sapiente provident quod expedita repellat in necessitatibus ipsum accusan</p>
         </header>
     );
 }
